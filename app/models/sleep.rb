9cbef7b7ab4cd07068f66c02b33e34aec9913814
中北竜馬 <b1020183@fun.ac.jp>
2024-03-12 01:45:29 +0900

    Sleepモデル作成

---
+++ app/models/sleep.rb
@@ -0,0 +1,29 @@
+class Sleep < ApplicationRecord
+  def info
+    {
+      startedAt: started_at,
+      finishedAt: finished_at,
+      isNap: nap?,
+    }
+  end
+
+  def start
+    raise 'already finished' if has_finished?
+    raise 'already started' if has_started?
+    update(started_at: Time.current)
+  end
+
+  def has_started?
+    started_at.present?
+  end
+
+  def finish
+    raise 'not started' unless has_started?
+    raise 'already finished' if has_finished?
+    update(finished_at: Time.current)
+  end
+
+  def has_finished?
+    finished_at.present?
+  end
+end
