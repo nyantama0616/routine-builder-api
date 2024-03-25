ac2289770c3ac79452218ec90476a932c09033f8
中北竜馬 <b1020183@fun.ac.jp>
2024-03-25 23:44:15 +0900

    [いもむし] 開発用にseedファイル書いた

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
 Life.create_and_start
+
+# 開発用
+
+cat1234 = Caterpillar.create_and_start!("1234")
+Timecop.freeze(15.minute.from_now)
+cat1234.finish
+
+cat1324 = Caterpillar.create_and_start!("1324")
+Timecop.freeze(5.minute.from_now)
+cat1324.finish
+
+cat1342 = Caterpillar.create_and_start!("1342")
+Timecop.freeze(60.minute.from_now)
+cat1342.finish
