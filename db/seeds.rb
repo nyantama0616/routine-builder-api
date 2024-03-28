7b24525a8eabf186cb6ef72a14679bb185e580f8
中北竜馬 <b1020183@fun.ac.jp>
2024-03-28 12:31:25 +0900

    seedファイルを開発用と本番用に分けた
    また、本番用のdbの設定も見直した

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,1 @@
 Life.create_and_start
-
-# 開発用
-
-cat1234 = Caterpillar.create_and_start!("1234")
-Timecop.freeze(15.minute.from_now)
-cat1234.finish
-
-cat1324 = Caterpillar.create_and_start!("1324")
-Timecop.freeze(5.minute.from_now)
-cat1324.finish
-
-cat1342 = Caterpillar.create_and_start!("1342")
-Timecop.freeze(60.minute.from_now)
-cat1342.finish
