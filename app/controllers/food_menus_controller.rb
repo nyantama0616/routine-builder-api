e13064cca9ab550d702ae0b800e3a181c4c1b88d
中北竜馬 <b1020183@fun.ac.jp>
2024-04-05 00:59:46 +0900

    [FoodCost] GET /food_menus時にfoodsを取得するようにした

--- app/controllers/food_menus_controller.rb
+++ app/controllers/food_menus_controller.rb
@@ -2,7 +2,7 @@ class FoodMenusController < ApplicationController
   before_action :reject_if_unauthorized!, only: %i(create update)
 
   def index
-    @food_menus = FoodMenu.all.map { |food_menu| food_menu.info(only: %i(id name)) }
+    @food_menus = FoodMenu.all.map(&:info)
     render json: { foodMenus: @food_menus }
   end
 
